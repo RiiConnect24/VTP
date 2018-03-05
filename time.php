2691c82b3c08ec0970e41188b1f9774f5c72100d
Larsenv <larsen@weborican.com>
2018-03-04 18:48:45 -0600

    Add Datadog to time

--- time.php
+++ time.php
@@ -1,8 +1,11 @@
 <?php
 
 if (isset($_SERVER['HTTP_USER_AGENT'])) // if not the Wii
-    die("Hi! You're not a Wii.");
+    die();
 
 date_default_timezone_set('GMT');
 header("Last-Modified: " . gmdate('D, d M Y H:i:s T'));
-header("Connection: keep-alive");
+
+require_once "vendor/autoload.php";
+
+DataDogStatsD::increment("votes.total_time_syncs");
